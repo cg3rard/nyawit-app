65e8cb627a7a9146573526510bf72fb9fe0cd804
Christian Gerard <cg3rard25@gmail.com>
2026-08-18 09:07:02 +0700

    Update docker-compose.yml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    watch: {
+      usePolling: true,
+    },
+  },
 });
